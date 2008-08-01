218f0aa36a131e867bd7de9c121616db1617d306
Garth Roxburgh-Kidd <garth@netapp.com>
2008-07-31 23:26:46 -0400

    Moved files to /var/puppet/python.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,36 +1,45 @@
 class python {
     package { "python": ensure => installed }
 
-    file { "/usr/bin/easy_install": ensure => present }
+    file { "/usr/bin/easy_install": }
 
     exec { "easy_install installer":
         creates => "/usr/bin/easy_install",
         before => File["/usr/bin/easy_install"],
-        require => File["/root/ez_setup.py"],
+        require => File["/var/puppet/python/ez_setup.py"],
         cwd => "/root",
-        command => "/bin/env python /root/ez_setup.py",
+        command => "/bin/env python /var/puppet/python/ez_setup.py",
         user => "root",
         logoutput => on_failure,
     }
 
-    file { "/root/ez_setup.py":
-        path => "/root/ez_setup.py",
-        source => "puppet:///python/ez_setup.py",
-        owner => "root",
-        group => "root",
-        mode => 0500,
-    }
-
-    file { "/var/puppet_pymod":
-        ensure => "directory",
-        owner => "root",
-        group => "root",
-        mode => 0755,
+    file {
+        "/var/puppet":
+            ensure => "directory",
+            owner => "root",
+            group => "root",
+            mode => 0755;
+        "/var/puppet/python":
+            ensure => "directory",
+            owner => "root",
+            group => "root",
+            mode => 0755;
+        "/var/puppet/python/modules":
+            ensure => "directory",
+            owner => "root",
+            group => "root",
+            mode => 0755;
+        "/var/puppet/python/ez_setup.py":
+            ensure => "present",
+            source => "puppet:///python/ez_setup.py",
+            owner => "root",
+            group => "root",
+            mode => 0500;
     }
 }
 
 define pymod($name) {
-    $pymod_record = "/var/puppet_pymod/$name.files"
+    $pymod_record = "/var/puppet/python/modules/$name.files"
     exec { "easy_install $name":
         cwd => "/root",
         creates => "$pymod_record",
